717063f8aade44cbfde867ae2b02be3d7ab0dfea
tejus <thequantumcoder@gmail.com>
2024-03-06 09:57:42 +0530

    ui : improve message styling

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -34,32 +34,77 @@ const Messages = ({initialMessages, sessionId}:MessagesProps) => {
           const hasNextMessageFromSameUser = messages[i - 1]?.senderId === messages[i]?.id;
 
           return (
-            <div key={`${message.id}-${message.timestamp}`}>
-              <div
-                className={cn(
-                  "flex flex-col space-y-2 text-base max-w-xs mx-2",
-                  {
-                    "order-1 items-end": isCurrentUser,
-                    "order-2 items-start": !isCurrentUser,
-                  }
-                )}
-              >
-                <span
-                  className={cn("px-4 py-2 rounded-lg inline-block", {
-                    "bg-indigo-600 text-white": isCurrentUser,
-                    "bg-gray-200 text-gray-900": !isCurrentUser,
-                    "rounded-br-none":
-                      !hasNextMessageFromSameUser && isCurrentUser,
-                    "rounded-bl-none":
-                      !hasNextMessageFromSameUser && !isCurrentUser,
-                  })}
-                >
-                  {message.text}{" "}
-                  <span className="ml-2 text-xs text-gray-400">
-                    {formatTimestamp(message.timestamp)}
-                  </span>
-                </span>
-              </div>
+            <div
+              id="messages"
+              className="flex h-full flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+            >
+              {/* <div ref={scrollDownRef} /> */}
+
+              {messages.map((message, index) => {
+                const isCurrentUser = message.senderId === sessionId;
+
+                const hasNextMessageFromSameUser =
+                  messages[index - 1]?.senderId === messages[index].senderId;
+
+                return (
+                  <div
+                    className="chat-message"
+                    key={`${message.id}-${message.timestamp}`}
+                  >
+                    <div
+                      className={cn("flex items-end", {
+                        "justify-end": isCurrentUser,
+                      })}
+                    >
+                      <div
+                        className={cn(
+                          "flex flex-col space-y-2 text-base max-w-xs mx-2",
+                          {
+                            "order-1 items-end": isCurrentUser,
+                            "order-2 items-start": !isCurrentUser,
+                          }
+                        )}
+                      >
+                        <span
+                          className={cn("px-4 py-2 rounded-lg inline-block", {
+                            "bg-indigo-600 text-white": isCurrentUser,
+                            "bg-gray-200 text-gray-900": !isCurrentUser,
+                            "rounded-br-none":
+                              !hasNextMessageFromSameUser && isCurrentUser,
+                            "rounded-bl-none":
+                              !hasNextMessageFromSameUser && !isCurrentUser,
+                          })}
+                        >
+                          {message.text}{" "}
+                          <span className="ml-2 text-xs text-gray-400">
+                            {formatTimestamp(message.timestamp)}
+                          </span>
+                        </span>
+                      </div>
+
+                      <div
+                        className={cn("relative w-6 h-6", {
+                          "order-2": isCurrentUser,
+                          "order-1": !isCurrentUser,
+                          invisible: hasNextMessageFromSameUser,
+                        })}
+                      >
+                        {/* <Image
+                          fill
+                          src={
+                            isCurrentUser
+                              ? (sessionImg as string)
+                              : chatPartner.image
+                          }
+                          alt="Profile picture"
+                          referrerPolicy="no-referrer"
+                          className="rounded-full"
+                        /> */}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           );
         })
